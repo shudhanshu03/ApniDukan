2b8ae15e533b2724e917bfa88f5c40447176297f
Shudhanshu Ranjan <sudhanshu03062002@gmail.com>
2022-02-05 14:22:45 +0530

    Few changes made

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -21,7 +21,7 @@ const Header = () => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>My Store</Navbar.Brand>
+            <Navbar.Brand>ApniDukan</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
